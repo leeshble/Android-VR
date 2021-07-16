1feb8833442890cb25368d8b454ec0f73101d69c
mikrono <qse365@gmail.com>
2021-07-17 00:59:59 +0900

    opencv streaming demo
    Opencv를 통한 플라스크 스트리밍의 데모

---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, Response
+import cv2
+
+#vid = cv2.VideoCapture(0) # 카메라 캡쳐
+vid = cv2.VideoCapture('ocp.mp4') # 파일 재생
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+def gen():
+    while True:
+        _, frame = vid.read()
+        rimg = cv2.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + rimg + b'\r\n')
+
+@app.route('/Feed')
+def Feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
